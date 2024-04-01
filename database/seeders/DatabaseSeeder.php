4bdd1cef99fc48d0c0a1ec9ffb354337ce4478be
leonardoolia <leonardooliaa@gmail.com>
2024-04-01 11:26:13 +0200

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(TypeSeeder::class);
+        $this->call(TechnologySeeder::class);
+        // \App\Models\User::factory(10)->create();
+        \App\Models\Project::factory(10)->create();
+
+        // Aggiungiamo il TypeSeeder
+
+        \App\Models\User::factory()->create([
+            'name' => 'Leonardo',
+            'email' => 'leonardo@esempio2.it',
+        ]);
+    }
+}
